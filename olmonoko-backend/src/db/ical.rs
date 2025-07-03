e47836ed9506aeaaa2d19d902984b8ce57a2a497
Elias Eskelinen <git@eliaseskelinen.fi>
2025-07-03 14:32:32 +0300

    fix: a minor case of major dt_end mishandling
    end dates (without time information) were discarded

--- olmonoko-backend/src/db/ical.rs
+++ olmonoko-backend/src/db/ical.rs
@@ -1,10 +1,12 @@
 //! Enhancements for the icalendar-rs library
 
 use chrono::{DateTime, Duration, NaiveTime, Utc};
+use chrono_tz::Tz;
 use icalendar::{Component, DatePerhapsTime};
 
 pub trait EnhancedIcalendarEvent {
-    fn get_end_auto(&self) -> Option<DateTime<Utc>>;
+    fn is_all_day(&self) -> bool;
+    fn get_end_auto(&self, src_tz: Tz) -> Option<DateTime<Utc>>;
     fn get_duration(&self) -> Option<Duration>;
 }
 
@@ -15,22 +17,31 @@ pub(crate) fn parse_duration(s: &str) -> Option<Duration> {
 }
 
 impl EnhancedIcalendarEvent for icalendar::Event {
+    fn is_all_day(&self) -> bool {
+        self.get_start()
+            .map(|dt| matches!(dt, DatePerhapsTime::Date(_)))
+            .unwrap_or(false) // events without occurrences aren't "all day" either
+    }
+
     fn get_duration(&self) -> Option<Duration> {
         parse_duration(self.properties().get("DURATION")?.value())
     }
 
-    fn get_end_auto(&self) -> Option<DateTime<Utc>> {
-        fn flatten_caldt(dt: DatePerhapsTime) -> Option<DateTime<Utc>> {
+    fn get_end_auto(&self, src_tz: Tz) -> Option<DateTime<Utc>> {
+        fn flatten_caldt(dt: DatePerhapsTime, src_tz: Tz) -> Option<DateTime<Utc>> {
             match dt {
-                DatePerhapsTime::Date(date) => date.and_time(NaiveTime::MIN).into(),
-                DatePerhapsTime::DateTime(dt) => dt,
+                DatePerhapsTime::Date(date) => date
+                    .and_time(NaiveTime::MIN)
+                    .and_local_timezone(src_tz)
+                    .earliest()
+                    .map(|dt| dt.to_utc()),
+                DatePerhapsTime::DateTime(dt) => dt.try_into_utc(),
             }
-            .try_into_utc()
         }
         if let Some(end) = self.get_end() {
-            return flatten_caldt(end);
+            return flatten_caldt(end, src_tz);
         }
-        let start = flatten_caldt(self.get_start()?)?;
+        let start = flatten_caldt(self.get_start()?, src_tz)?;
         let duration = self.get_duration()?;
         let end = start + duration;
         Some(end)
